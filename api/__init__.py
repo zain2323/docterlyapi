44636b65130019492e9242171f00680154369393
zain2323 <zainsiddiqui2323@gmail.com>
2022-06-01 09:20:07 -0700

    Added configuration and setup flask-migrate

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask_migrate import Migrate
+from api.config import DevelopmentConfig as Config
 
 api_fairy = APIFairy()
 ma = Marshmallow()
@@ -12,10 +13,11 @@ basic_auth = HTTPBasicAuth()
 token = HTTPTokenAuth()
 migrate = Migrate()
 
-def create_app():
+def create_app(config=Config):
     app = Flask(__name__)
+    app.config.from_object(Config)
     initialize_extensions(app)
-    initialize_extensions(app)
+    register_blueprints(app)
     return app
 
 def initialize_extensions(app):
---
+++ api/config.py
@@ -0,0 +1,29 @@
+from urllib.parse import quote
+import json
+
+# class ProductionConfig:
+#     with open("/etc/secrets.json", "r") as secrets_file:
+#         config = json.load(secrets_file)
+#     SECRET_KEY = config.get("SECRET_KEY")
+#     UPLOAD_FOLDER = './api/static/results'
+#     MAIL_SERVER = "smtp.googlemail.com"
+#     MAIL_PORT = "587"
+#     MAIL_USE_TLS = "1"  
+#     MAIL_USERNAME = config.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = config.get("MAIL_PASSWORD")
+#     ADMINS = [config.get("EMAIL")]
+#     SUBSCRIPTION_KEY = config.get("SUBSCRIPTION_KEY")
+#     ENDPOINT = config.get("ENDPOINT")
+#     REDIS_URL = 'redis://'
+#     FLASK_ENV = "production"
+#     TESTING = False
+#     SQLALCHEMY_DATABASE_URI = f'postgresql://{config.get("DB_ROLE")}:%s@{config.get("HOST")}/{config.get("DB_NAME")}' % quote(config.get("DB_PASSWORD"))
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig:
+    FLASK_ENV = "development"
+    SECRET_KEY = "'0035ce4c6d1f0d6f327d97051b9bcfafc7d922b2ce06c4f7dbff5ee4d159b50f'"
+    FLASK_DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
